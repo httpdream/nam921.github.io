863cc52baded21d1bc7b692aa756317d1fcf0f4b
Kim namHwan <nam9212006@naver.com>
2014-09-27 17:32:44 +0900

    test1
    실제로 호스팅이 되는지 확인.

---
+++ DATA/JS/ALTIS.js
@@ -0,0 +1,252 @@
+/***********
+ALTIS FRAMEWORK
+2014.09.21: v0.01
+            파일 생성,
+            생성자,
+            이미지 관련 함수 loadImage, addImage,
+            애니메이션 관련 함수loadSprite, addSprite, setSprite, getSprite, showSprite
+            텍스트, 사각형 추가 함수 addText, addRect 구현.
+2014.09.22: v0.02
+            키입력 관련 함수 keyCode, addKeyDown, addKeyUp 구현.
+2014.09.23: v0.03
+            데이터를 저장할 수단인 saveData, getData, delData 구현
+2014.09.24: v0.04
+            버튼 관련 함수 addButton 구현
+***********/
+
+
+var keyCode = {
+    //Arrow
+    37: 'LEFT',
+    38: 'UP',
+    39: 'RIGHT',
+    40: 'DOWN',
+
+    //Number
+    48: 'ZERO',
+    49: 'ONE',
+    50: 'TWO',
+    51: 'THREE',
+    52: 'FOUR',
+    53: 'FIVE',
+    54: 'SIX',
+    55: 'SEVEN',
+    56: 'EIGHT',
+    57: 'NINE',
+
+    //Alahabet
+    65: 'A',
+    66: 'B',
+    67: 'C',
+    68: 'D',
+    69: 'E',
+    70: 'F',
+    71: 'G',
+    72: 'H',
+    73: 'I',
+    74: 'J',
+    75: 'K',
+    76: 'L',
+    77: 'M',
+    78: 'N',
+    79: 'O',
+    80: 'P',
+    81: 'Q',
+    82: 'R',
+    83: 'S',
+    84: 'T',
+    85: 'U',
+    86: 'V',
+    87: 'W',
+    88: 'X',
+    89: 'Y',
+    90: 'Z',
+
+    //Function Key
+    112: 'F1',
+    113: 'F2',
+    114: 'F3',
+    115: 'F4',
+    116: 'F5',
+    117: 'F6',
+    118: 'F7',
+    119: 'F8',
+    120: 'F9',
+    121: 'F10',
+    122: 'F11',
+    123: 'F12'
+};
+
+function ALTIS(id, width, height, visible) {
+    this.width = width;
+    this.height = height;
+    this.Image = new Array();
+    this.Sprite = new Array();
+
+    if(visible == true)
+        document.body.innerHTML += '<canvas id = ' + id + ' width=' + width + ' height=' + height + ' style="position: absoulte;">캔버스도 안되</canvas>';
+    else
+        document.body.innerHTML += '<canvas id = ' + id + ' width=' + width + ' height=' + height + ' style="position: absoulte; display:none;">캔버스도 안되</canvas>';
+    
+    this.Context = document.getElementById(id).getContext("2d");
+
+    return this;
+}
+
+Array.prototype.Last = function(){
+    return this[this.length-1];
+}
+
+ALTIS.prototype.loadImage = function (src, name){
+    this.Image.push({ img: new Image(), name: name, load: 0});
+    var thisImage = this.Image.Last();
+    thisImage.img.addEventListener('load', function () { thisImage.load = 1; }, false);
+    thisImage.img.src = src;
+}
+
+ALTIS.prototype.loadSprite = function (src, name, width, height, delay) {
+    this.Sprite.push({ img: new Image(), name: name, width: width, height: height, delay: delay, count: 0, curFrame: 0, sprite: Array(), current: 0, load: 0 });
+    var thisSprite = this.Sprite.Last();
+    thisSprite.img.addEventListener('load', function () { thisSprite.load = 1; }, false);
+    thisSprite.img.src = src;
+}
+
+ALTIS.prototype.addSprite = function (src_name, sprite_name, sprite_frames) {
+    var found = 0;
+    for (var i = 0; i < this.Sprite.length; i++) {
+        if (src_name == this.Sprite[i].name) {
+            this.Sprite[i].sprite.push({ src_name: sprite_name, frame: sprite_frames });
+            break;
+        }
+    }
+}
+
+ALTIS.prototype.setSprite = function (src_name, sprite_name) {
+    var find = 0;
+    for (var i = 0; i < this.Sprite.length; i++) {
+        if (src_name == this.Sprite[i].name) {
+            for (var j = 0; j < this.Sprite[i].sprite.length; j++) {
+                if (sprite_name == this.Sprite[i].sprite[j].src_name) {
+                    if (this.Sprite[i].current != j) {
+                        this.Sprite[i].current = j;
+                        this.Sprite[i].curFrame = 0;
+                        find = 1;
+                    }
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    if (!find) console.log('헐 못찾음');
+}
+
+ALTIS.prototype.getSprite = function (src_name) {
+    var sprt;
+    for (var i = 0; i < this.Sprite.length; i++) {
+        if (src_name == this.Sprite[i].name) {
+            sprt = this.Sprite[i];
+        }
+    }
+    return sprt;
+}
+
+ALTIS.prototype.addText = function (text, font, x, y, color) {
+    this.Context.fillStyle = color;
+    this.Context.font = font;
+    this.Context.fillText(text, x, y);
+}
+
+ALTIS.prototype.addRect = function (x, y, width, height, color) {
+    this.Context.fillStyle = color;
+    this.Context.fillRect(x, y, width, height);
+}
+
+ALTIS.prototype.addImage = function (src_name, x, y, width, height, callback) {
+    var found;
+    for (var i = 0; i < this.Image.length; i++) {
+        if (src_name == this.Image[i].name) {
+            found = this.Image[i].img;
+            break;
+        }
+    }
+    if (!found) {
+        //found = this.none;
+    }
+
+    return {
+        Context: this.Context, play: function (to_x, to_y) {
+            this.Context.drawImage(found, to_x, to_y, width, height);
+        }
+    };
+}
+
+ALTIS.prototype.showSprite = function (src_name, x, y, other_frame) {
+    var found = 0;
+    for (var i = 0; i < this.Sprite.length; i++) {
+        if (src_name == this.Sprite[i].name) {
+            var spriteX = Math.floor(this.Sprite[i].sprite[this.Sprite[i].current].frame[this.Sprite[i].curFrame] % other_frame) * this.Sprite[i].width;
+            var spriteY = Math.floor(this.Sprite[i].sprite[this.Sprite[i].current].frame[this.Sprite[i].curFrame] / other_frame) * this.Sprite[i].height;
+            this.Context.drawImage(this.Sprite[i].img, spriteX, spriteY, this.Sprite[i].width, this.Sprite[i].height, x, y, this.Sprite[i].width, this.Sprite[i].height);
+            if (this.Sprite[i].count++ > this.Sprite[i].delay) { this.Sprite[i].curFrame++; this.Sprite[i].count = 0; }
+            if (this.Sprite[i].curFrame >= this.Sprite[i].sprite[this.Sprite[i].current].frame.length) this.Sprite[i].curFrame = 0;
+            found = 1;
+            break;
+        }
+    }
+    if (found == 0) {
+        this.Context.drawImage(this.none, x, y, 80, 80);
+    }
+}
+
+ALTIS.prototype.addKeyDown = function (key, callback) {
+    var code;
+    for (var i in keyCode) {
+        if (keyCode.hasOwnProperty(i))
+            if (key == keyCode[i]) code = i;
+    }
+    
+    if (!code) console.log('not found');
+
+    window.addEventListener('keydown', function (e) {
+        if (e.keyCode == code) callback();
+    }, false);
+}
+
+ALTIS.prototype.addKeyUp = function (key, callback) {
+    var code;
+    for (var i in keyCode) {
+        if (keyCode.hasOwnProperty(i))
+            if (key == keyCode[i]) code = i;
+    }
+
+    if (!code) console.log('not found');
+
+    window.addEventListener('keyup', function (e) {
+        if (e.keyCode == code) callback();
+    }, false);
+}
+
+ALTIS.prototype.saveData = function(name, data){
+    localStorage.setItem(name, data);
+}
+
+ALTIS.prototype.getData = function(name){
+    return localStorage.getItem(name);
+}
+
+ALTIS.prototype.delData = function(name){
+    localStorage.removeItem(name);
+}
+
+ALTIS.prototype.setWidth = function(w){
+    this.width = w;
+}
+
+ALTIS.prototype.setHeight = function(h){
+    this.height = h;
+}
+
+ALTIS.prototype.clear = function () {
+    this.Context.clearRect(0, 0, this.width, this.height);
+}
---
+++ DATA/JS/FRAMEWORK.js
@@ -0,0 +1,305 @@
+/******************
+TEST GAME
+2014.09.21: v0.01
+            파일 생성.
+            맵 로드.
+            캐릭터 상하좌우 움직임.
+            카메라 구현.
+2014.09.22: v0.02
+            포탈 기능의 프로토타입 구현.
+2014.09.23: v0.03
+            쿠키를 이용한 시나리오 저장기능 구현.
+2014.09.25: v0.04
+            동적으로 캔버스의 사이즈가 결정됨.
+            사이즈에 따라서 카메라가 동적으로 바뀜.
+2014.09.26: v0.05
+            쿠키는 자동삭제가 가능하므로 localStorage에 저장하는 것으로 변경.
+            getImageData함수를 이용하여 장애물 충돌시 멈추기, 노란부분, 파란부분일 때 다른 맵 이동
+2014.09.27: v0.06
+            시작페이지의 프로토타입 구현.
+******************/
+
+window.addEventListener("load", onPageLoadComplete, false);
+var framework;
+var Temp; //충돌처리
+//var Interface;
+
+var temp;
+var imgData;
+
+var gamestate = 0;
+var STATE_START = 0;
+var STATE_PLAY = 1;
+var STATE_OPEN = 2;
+var STATE_CUSTOM = 3;
+var STATE_GALALY = 4;
+var STATE_MAKER = 5;
+var potal;
+var potaling = 0;
+
+var loadInterval;
+var char_status = { up: false, down: false, left: false, right: false };
+var MAP = new Array();
+var TEMP_MAP = new Array();
+var bg_y = 0;
+var bg_x = 0;
+var player_y = 0;
+var player_x = 0;
+var loadInterval;
+var MAP_CODE = 0;
+var CURRENT_MAP;
+var moveable = true;
+
+var width = 0;
+var height = 0;
+var loader;
+
+function draw_load() {
+    loader = 0;
+
+    for (var i = 0; i < framework.Image.length; i++) {
+        loader += framework.Image[i].load;
+    }
+    
+    for (var i = 0; i < Temp.Image.length; i++) {
+        loader += Temp.Image[i].load;
+    }
+
+    for (var i = 0; i < framework.Sprite.length; i++) {
+        loader += framework.Sprite[i].load;
+    }
+
+
+    framework.clear();
+    framework.addText('이미지 리소스 로딩중 (' + loader + '/' + (framework.Image.length + framework.Sprite.length + Temp.Image.length) + ')...', '20px nanumgothiccoding', 150, 180, '#000');
+
+    if (loader == (framework.Image.length + framework.Sprite.length + Temp.Image.length)) {
+        start_load();
+        setInterval(gameLoop, 1000 / 60);
+        clearInterval(loadInterval);
+    }
+}
+
+function onPageLoadComplete() {
+    var FPS = 60;
+    width = $(window).innerWidth()-32;
+    height = $(window).innerHeight()-32;
+    
+    Temp = new ALTIS('Temp', 2000, 2000, false);
+    framework = new ALTIS('ALTIS', width, height, true);
+    framework.loadImage('DATA/IMAGE/MAP/bg_npcnpc.png', 'MAP1');
+    Temp.loadImage('DATA/IMAGE/MAP/bg_temp.png', 'MAP1');
+    Temp.loadImage('DATA/IMAGE/MAP/picture.jpg', 'MAP2');
+    framework.loadImage('DATA/IMAGE/MAP/picture.jpg', 'MAP2');
+    framework.loadSprite('DATA/IMAGE/CHARACTER/spp.png', 'char', 31, 48, 10);
+
+    loadInterval = setInterval(draw_load, 1000 / FPS);
+}
+
+//시작페이지
+function start_load(){
+    
+}
+
+//본게임
+function start_game() {
+    framework.clear();
+
+    MAP.push({map: framework.addImage('MAP1', 0, 0, 1024, 768), width: 1024, height: 768});
+    MAP.push({map: framework.addImage('MAP2', 0, 0, 1920, 1280), width: 1920, height: 1280});
+    TEMP_MAP.push({map: Temp.addImage('MAP1', 0, 0, 1024, 768), width: 1024, height: 768});
+    TEMP_MAP.push({map: Temp.addImage('MAP1', 0, 0, 1920, 1280), width: 1920, height: 1280});
+    
+
+    framework.addSprite('char', 'left', [4, 5, 6, 7]);
+    framework.addSprite('char', 'right', [8, 9, 10, 11]);
+    framework.addSprite('char', 'up', [12, 13, 14, 15]);
+    framework.addSprite('char', 'down', [0, 1, 2, 3]);
+    framework.addSprite('char', 'stand_left', [4]);
+    framework.addSprite('char', 'stand_right', [8]);
+    framework.addSprite('char', 'stand_up', [12]);
+    framework.addSprite('char', 'stand_down', [0]);
+
+    framework.setSprite('char', 'stand_down');
+
+    framework.addKeyDown('LEFT', function () {
+        char_status.left = true;
+        framework.setSprite('char', 'left');
+    });
+
+    framework.addKeyDown('RIGHT', function () {
+        char_status.right = true;
+        framework.setSprite('char', 'right');
+    });
+
+    framework.addKeyDown('UP', function () {
+        char_status.up = true;
+        framework.setSprite('char', 'up');
+    });
+
+    framework.addKeyDown('DOWN', function () {
+        char_status.down = true;
+        framework.setSprite('char', 'down');
+    });
+
+    framework.addKeyUp('LEFT', function () {
+        char_status.left = false;
+        framework.setSprite('char', 'stand_left');
+    });
+
+    framework.addKeyUp('RIGHT', function () {
+        char_status.right = false;
+        framework.setSprite('char', 'stand_right');
+    });
+
+    framework.addKeyUp('UP', function () {
+        char_status.up = false;
+        framework.setSprite('char', 'stand_up');
+    });
+
+    framework.addKeyUp('DOWN', function () {
+        char_status.down = false;
+        framework.setSprite('char', 'stand_down');
+    });
+    
+    ///COOKIE TEST
+    framework.addKeyDown('A', function(){
+        framework.saveData('save', '3b45ssae');
+    });
+    framework.addKeyDown('B', function(){
+        framework.saveData('save', 'p5j2h1q');
+    });
+    framework.addKeyDown('C', function(){
+        framework.saveData('save', 'mj7g2vd');
+    });
+    framework.addKeyDown('F', function(){
+        alert(framework.getData('save'));
+    });
+    framework.addKeyDown('D', function(){
+        framework.delData('save');
+    });
+}
+
+//Potal(MAP_CODE, player_x, player_y, bg_x, bg_y);
+function Potal(MAP_Code, pl_x, pl_y, bgx, bgy){
+    MAP_CODE = MAP_Code;
+    CURRENT_MAP = MAP[MAP_Code];
+    player_y = pl_x;
+    player_x = pl_y;
+    framework.Context.translate(bg_x, bg_y);
+    bg_x = bgx;
+    bg_y = bgy;
+    
+    moveable = false;
+    var potal = setInterval(function () {
+        potaling += 0.01;
+        framework.Context.globalAlpha = potaling;
+        if (potaling > 1) {
+            potaling = 0;
+            moveable = true;
+            clearInterval(potal);
+        }
+            
+    }, 1000 / 60);
+}
+
+//Temp와 현재 캐릭터의 위치를 비교
+function check(x, y, moved){
+    //character width: 31, width: 48
+    var imgData = Temp.Context.getImageData(player_x, player_y, 31, 48);
+    for(var i=0; i<imgData.data.length; i+=4){
+        var r = imgData.data[i];
+        var g = imgData.data[i+1];
+        var b = imgData.data[i+2];
+        
+        if(r!=255 || g!=255 || b!=255){
+            if(!moved){
+                framework.Context.translate(-x, -y);
+                bg_x+=x;
+                bg_y+=y;
+            }
+            player_x+=x;
+            player_y+=y;
+            
+            console.log('r: ' + r + 'g: ' + g + 'b: ' + b);
+
+            switch(MAP_CODE){
+                case 0:
+                    if(r==115 && g==78 && b==69){
+                        Potal(1, 0, 0, 0, 0);
+                        console.log('hi~');
+                    }
+                    break;
+            }
+            break;
+        }
+        
+    }
+}
+
+var potaling = 0;
+function Update() { }
+function Render() {
+    switch(gamestate){
+        case STATE_PLAY:
+            if(moveable){
+                if (char_status.up) {
+                    player_y -= 3;
+                    if(bg_y > 0 && bg_y+height/2>player_y){
+                        bg_y -= 3;
+                        framework.Context.translate(0, 3);
+                        check(0, 3, false);
+                    }
+                    else check(0, 3, true);
+                }
+
+                else if (char_status.down) {
+                    player_y += 3;
+                    if(bg_y < MAP[MAP_CODE].height-height && bg_y+height/2<player_y){
+                        bg_y += 3;
+                        framework.Context.translate(0, -3);
+                        check(0, -3, false);
+                    }
+                    else check(0, -3, true);
+                }
+                else if (char_status.left) {
+                    player_x -= 3;
+                    if(bg_x > 0 && bg_x+width/2>player_x){
+                        framework.Context.translate(3, 0);
+                        bg_x -= 3;
+                        check(3, 0, false);
+                    }
+                    check(3, 0, true);
+                }        
+                else if(char_status.right){
+                    player_x += 3;
+                    if(bg_x < MAP[MAP_CODE].width-width && bg_x+width/2<player_x){
+                        bg_x+=3;
+                        check(-3, 0, false);
+                    }
+                    else check(-3, 0, true);
+                }
+            }
+
+            framework.clear();
+            Temp.clear();
+
+    
+            TEMP_MAP[MAP_CODE].map.play(0, 0);
+            MAP[MAP_CODE].map.play(0, 0);
+
+            framework.showSprite('char', player_x, player_y, 4);
+            break;
+            
+        case STATE_OPEN:
+            break;
+        case STATE_START:
+            framework.addRect(0,0,width, height, '#0f0');
+            break;
+    }
+}
+
+function gameLoop() {
+    Update();
+    Render();
+}
