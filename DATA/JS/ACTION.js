669b718fe63cdd4da2141be7d03fe4cd52a5c46e
nam921 <nam9212006@naver.com>
2014-10-07 17:39:49 +0900

    @@
    @@

--- DATA/JS/ACTION.js
+++ DATA/JS/ACTION.js
@@ -1,9 +1,20 @@
-function ACTION(npc, callback, r, g, b) {
+function ACTION(map, npc, action, r, g, b) {
     this.status = 0;
     this.npc = npc;
+    var.map = map;
+    
+    if(r==119 && g==149 && b==217){
+                        addSpaceDown(_x, _y, current, function(){
+                            Potal(1,100,100,0,0);
+                            around = 1;
+                        });
+                    }
+    
     this.r = r;
     this.g = g;
     this.b = b;
+    
+    this.action = action;
     
     return this;
 }
