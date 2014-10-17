9ce6788fd59f0780b797ce97d73e0a278d2dfa61
nam921 <nam9212006@naver.com>
2014-10-18 04:20:09 +0900

    대소문자구분이라니
    ㅜㅜ

--- DATA/JS/resource.js
+++ DATA/JS/resource.js
@@ -6,13 +6,13 @@ var Sprite_Array = new Array();
 Map_Array.push({src: 'MAP/Room.png', name: 'MAP1', width: 1600, height: 900, npc: [0,3], auto: 2});
 Map_Array.push({src: 'MAP/Map_Room.png', name: 'MAP2', width: 1920, height: 1080, npc: [1]});
 
-Map_Tmp_Array.push({src: 'MAP/_Room.png', name: 'MAP1', width: 1600, height: 900});
+Map_Tmp_Array.push({src: 'MAP/TMP_Room.png', name: 'MAP1', width: 1600, height: 900});
 Map_Tmp_Array.push({src: 'MAP/Temp_Map_Room.png', name: 'MAP2', width: 1920, height: 1080});
 
 Illust_Array.push({src: 'ILLUST/alpha.png', name: 'ILLUST1'});
 Illust_Array.push({src: 'ILLUST/beta.png', name: 'ILLUST2'});
 Illust_Array.push({src: 'ILLUST/lucid.png', name: 'LUCID'});
-Illust_Array.push({src: 'ILLUST/research.png', name: 'RESEARCH'});
+Illust_Array.push({src: 'ILLUST/Research.png', name: 'RESEARCH'});
 
 Sprite_Array.push({src: 'CHARACTER/sppp.png', name: 'char', width: 53.25, height: 80, delay: 10})
 
